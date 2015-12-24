126b829a3e98dfe3a3de10efc1e89b7c9eaa840d
xiaolyuh <xiaolyuh@163.com>
2015-12-24 14:42:36 +0800

    优化没有实现类的情况下 添加注解出错

--- dds/src/main/java/cn/smart/dds/DataSourceAspect.java
+++ dds/src/main/java/cn/smart/dds/DataSourceAspect.java
@@ -15,29 +15,36 @@ public class DataSourceAspect {
         Object target = point.getTarget();
         String method = point.getSignature().getName();
 
-        //获取接口
+        // 注解加在接口上
         Class<?>[] interfaces = target.getClass().getInterfaces();
         
         Class<?>[] parameterTypes = ((MethodSignature) point.getSignature())
                 .getMethod().getParameterTypes();
         try {
         	Method m = null;
-        	if (interfaces != null && interfaces.length > 0) {
-        		// 先找接口
-        		m = interfaces[0].getMethod(method, parameterTypes);
-			} else {
-				// 接口不存在的情况下再找实现类
-				m = target.getClass().getMethod(method, parameterTypes);
+	        // 先判断接口是否存在
+			if (interfaces != null && interfaces.length > 0) {
+				m = interfaces[0].getMethod(method, parameterTypes);
 			}
-            if (m != null && m.isAnnotationPresent(DataSource.class)) {
-                DataSource data = m
-                        .getAnnotation(DataSource.class);
+        	
+			// 如果接口存在 再判断注解是否存在
+			if (m != null && m.isAnnotationPresent(DataSource.class)) {
+                DataSource data = m.getAnnotation(DataSource.class);
                 DynamicDataSourceHandler.putDataSource(data.value());
                 logger.debug(data.value());
             } else {
-            	// 设置默认注解值（主库）
-            	DynamicDataSourceHandler.putDataSource(DataSource.MASTER);
-            	logger.debug("Default DataSourcee is Master");
+            	// 如果接口不存在或者接口上的注解不存在时，找对应的类上注解
+            	m = target.getClass().getMethod(method, parameterTypes);
+            	
+            	if (m != null && m.isAnnotationPresent(DataSource.class)) {
+                    DataSource data = m.getAnnotation(DataSource.class);
+                    DynamicDataSourceHandler.putDataSource(data.value());
+                    logger.debug(data.value());
+                } else {
+                	// 设置默认注解值（主库）
+                	DynamicDataSourceHandler.putDataSource(DataSource.MASTER);
+                	logger.debug("Default DataSourcee is Master");
+                }
             }
             
         } catch (Exception e) {
